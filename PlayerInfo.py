0da4b9c335be1b3596ee491fee347414683096fe
sanathkuppa <sanathkuppa@gmail.com>
2017-03-08 08:16:36 -0500

    Update PlayerInfo.py
    Credits - Sabareesh Nikhil
    
    Done till now:
    1. Massive beautification of the code
    2. Move everything into functions
    3. Read the data into a data frame
    
    To be done next:
    1. Loop over all the pages to get the data for all players
    2. Try using just 1 xpath call for each table
    3. Fix the issue of not getting info when a player is injured or suspended

--- PlayerInfo.py
+++ PlayerInfo.py
@@ -1,74 +1,77 @@
 import time
+import logging
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+from collections import namedtuple
 
+logger = logging.getLogger()
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(levelname)-8s %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-# Go to the required webpage 
-driver = webdriver.Chrome("C:/Python/chromedriver.exe")
-driver.get('https://fantasy.premierleague.com/a/statistics/total_points')
-driver.implicitly_wait(10) # seconds
+# Go to the required webpage and login
+chromedriver = "/Users/SabareeshNikhil/Downloads/chromedriver"
+url = 'https://fantasy.premierleague.com/a/statistics/total_points'
 
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, u"//*[@id='ismr-main']/div/div[1]/table/tbody/tr[1]/td[1]/a"))
-        
+# Dict to map xpath of each stat
+XPATH = dict(club="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/div[1]",
+                position="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/div[3]",
+                form="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[1]/div",
+                last_week_score="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[2]/div",
+                total_score="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[3]/div",
+                price="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[4]/div",
+                teams_sel_by="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[5]/div",
+                net_influence="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[2]/li[1]/div",
+                net_creativity="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[2]/li[2]/div",
+                net_threat="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[2]/li[3]/div",
+                net_ict="//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[2]/li[4]/div"
     )
-    element.click()
 
-    # Get the main attributes for the first player - Should be made neat later 
-    name = driver.find_element_by_class_name('ism-eiw-heading')
-    print(name.text) # Player name
-    club = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/div[1]")
-    print(club.text)
-    position = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/div[3]")
-    print(position.text)
-    form = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[1]/div")
-    print(form.text)
-    LastWeekScore = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[2]/div")
-    print(LastWeekScore.text)
-    TotalScore = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[3]/div")
-    print(TotalScore.text)
-    Price = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[4]/div")
-    print(Price.text)
-    TSB = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[1]/li[5]/div")
-    print(TSB.text)
-    Influence = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[2]/li[1]/div")
-    print(Influence.text)
-    Creativity = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[2]/li[2]/div")
-    print(Creativity.text)
-    Threat = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[2]/li[3]/div")
-    print(Threat.text)
-    ICT = driver.find_element_by_xpath("//*[@id='ismr-element']/div/div[2]/div[1]/div[1]/ul[2]/li[4]/div")
-    print(ICT.text)
+MatchData = list(['pno', 'matchweek', 'scoreline', 'points', 'minsplayed', 'goals', 'assists', 'cleansheets', 'goalsconceded', 'owngoals', 'penssaved', 'pensmissed', 'yellowcards', 'redcards', 'saves', 'bonus', 'bonuspts', 'influence', 'creativity', 'threat', 'ict', 'nettransfers', 'selectedby', 'value'])
+df = pd.DataFrame(columns=XPATH.keys()+MatchData)
 
-    # Main Data for the first player
-    DataTable = driver.find_element_by_xpath("//*[@id='ismr-element-history-this']/div/div/table")
-    TableRows = DataTable.find_elements_by_xpath("//*[@id='ismr-element-history-this']/div/div/table/tbody/tr")
-    print(len(TableRows))
+#MatchData = namedtuple('MatchData', ['Opponent', 'Venue', 'TeamScore', 'OpponentScore', 'MinsPlayed', 'Goals', 'Assists', 'CleanSheets', 'GoalsConceded', 'OwnGoals', 'PensSaved', 'PensMissed', 'YellowCards', 'RedCards', 'Saves', 'Bonus', 'BonusPts', 'Influence'])
+#PlayerData = namedtuple('PlayerData', ['PlayerNo', 'Name', 'Club', 'Position', 'Form', 'LastWeekScore', 'TotalScore', 'Price', 'TeamsSelBy', 'NetInfluence', 'Creativity', 'Threat', 'ICT', 'MatchList'])
 
-    # Printing the main data for the first player
-##    for i in range(len(TableRows)):
-##        TableCols = driver.find_elements_by_xpath("//*[@id='ismr-element-history-this']/div/div/table/tbody/tr["+str(i+1)+"]/td")
-##        print(len(TableCols))
-##        for j in range(len(TableCols)):
-##            TableElem = driver.find_element_by_xpath("//*[@id='ismr-element-history-this']/div/div/table/tbody/tr["+str(i+1)+"]/td["+str(j+1)+"]")
-##            print(TableElem.text)
+def open_page(chromedriver, url) :
+    """Function to call chromedriver and open webpage after authenticating"""
+    driver = webdriver.Chrome(chromedriver)
+    driver.get(url)
+    driver.implicitly_wait(10) # seconds
+    logger.info("Opened webpage %s to read player stats" % url)
+    return driver
 
-    # Close the first player's dialog
-    close = driver.find_element_by_xpath('//*[@id="ismr-element"]/div/div[1]/a')
-    close.click()
+def read_data(driver) :
+    try:
+        count = 0
+        # Get all the players in the 1st page and print their names. Should modify the loop below so that it extract the other information as well and stores in a table
+        players = driver.find_elements_by_xpath("//*[@id='ismr-main']/div/div[1]/table/tbody/tr")
+        #for pno in range(len(players)):
+        for pno in range(20):
+            Button = driver.find_element_by_xpath("//*[@id='ismr-main']/div/div[1]/table/tbody/tr["+str(pno+1)+"]/td[1]/a")
+            Button.click()
+            name = driver.find_element_by_class_name('ism-eiw-heading')
+            pdata = [driver.find_element_by_xpath(XPATH[stat]).text.encode('ascii', 'ignore') for stat in XPATH.keys()]
 
-    # Get all the players in the 1st page and print their names. Should modify the loop below so that it extract the other information as well and stores in a table
-    players = driver.find_elements_by_xpath("//*[@id='ismr-main']/div/div[1]/table/tbody/tr")
-    for i in range(len(players)):
-        Button = driver.find_element_by_xpath("//*[@id='ismr-main']/div/div[1]/table/tbody/tr["+str(i+1)+"]/td[1]/a")
-        Button.click()
-        name = driver.find_element_by_class_name('ism-eiw-heading')
-        print(name.text) # Player name
-        close = driver.find_element_by_xpath('//*[@id="ismr-element"]/div/div[1]/a')
-        close.click()
-finally:
-    print("blah")
+            DataTable = driver.find_element_by_xpath("//*[@id='ismr-element-history-this']/div/div/table")
+            TableRows = DataTable.find_elements_by_xpath("//*[@id='ismr-element-history-this']/div/div/table/tbody/tr")            
+            for row in range(len(TableRows)):
+                TableCols = driver.find_elements_by_xpath("//*[@id='ismr-element-history-this']/div/div/table/tbody/tr["+str(row+1)+"]/td")
+                mdata= [pno] + [driver.find_element_by_xpath("//*[@id='ismr-element-history-this']/div/div/table/tbody/tr["+str(row+1)+"]/td["+str(col+1)+"]").text.encode('ascii', 'ignore') for col in range(len(TableCols))]
+                df.loc[count] = pdata + mdata
+                count += 1
+            close = driver.find_element_by_xpath('//*[@id="ismr-element"]/div/div[1]/a')
+            close.click()        
+    finally:
+        return df
+
+
+driver = open_page(chromedriver, url)
+df = read_data(driver)
+print(df)
