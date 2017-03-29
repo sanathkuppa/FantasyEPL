d99e69b93e833a0366136c33d91233b00475ef6e
Sanath Kuppa <sanathkuppa@gmail.com>
2017-03-29 01:12:00 -0400

    Update PlayerInfo.py
    Added multithreading.. Open one chrome window for each page.. 
    
    Data analysis to come up next!

--- PlayerInfo.py
+++ PlayerInfo.py
@@ -1,3 +1,4 @@
+import threading
 import time
 import logging
 import pandas as pd
@@ -15,9 +16,13 @@ formatter = logging.Formatter("%(levelname)-8s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# Go to the required webpage and login
-chromedriver = "/Users/SabareeshNikhil/Downloads/chromedriver"
-chromedriver = "C:/Python/chromedriver.exe"
+
+#Array of ChromeDrivers
+Initial         = 15
+Final           = 18
+WindowCount     = Final - Initial + 1
+chromedriver    = "/Users/SabareeshNikhil/Downloads/chromedriver"
+chromedriver    = "C:/Python/chromedriver.exe"
 
 url = 'https://fantasy.premierleague.com/a/statistics/total_points'
 
@@ -35,61 +40,83 @@ XPATH = dict(   club            = ["//*[@id='ismr-element']/div/div[2]/div[",1,"
                 net_ict         = ["//*[@id='ismr-element']/div/div[2]/div[",1,"]/div[1]/ul[2]/li[4]/div"]
     )
 
-MatchData = list(['pno', 'matchweek', 'scoreline', 'points', 'minsplayed', 'goals', 'assists', 'cleansheets', 'goalsconceded', 'owngoals', 'penssaved', 'pensmissed', 'yellowcards', 'redcards', 'saves', 'bonus', 'bonuspts', 'influence', 'creativity', 'threat', 'ict', 'nettransfers', 'selectedby', 'value'])
-df = pd.DataFrame(columns=list(XPATH.keys())+MatchData)
+MatchData = list(['pno', 'name', 'matchweek', 'scoreline', 'points', 'minsplayed', 'goals', 'assists', 'cleansheets', 'goalsconceded', 'owngoals', 'penssaved', 'pensmissed', 'yellowcards', 'redcards', 'saves', 'bonus', 'bonuspts', 'influence', 'creativity', 'threat', 'ict', 'nettransfers', 'selectedby', 'value'])
 
 #MatchData = namedtuple('MatchData', ['Opponent', 'Venue', 'TeamScore', 'OpponentScore', 'MinsPlayed', 'Goals', 'Assists', 'CleanSheets', 'GoalsConceded', 'OwnGoals', 'PensSaved', 'PensMissed', 'YellowCards', 'RedCards', 'Saves', 'Bonus', 'BonusPts', 'Influence'])
 #PlayerData = namedtuple('PlayerData', ['PlayerNo', 'Name', 'Club', 'Position', 'Form', 'LastWeekScore', 'TotalScore', 'Price', 'TeamsSelBy', 'NetInfluence', 'Creativity', 'Threat', 'ICT', 'MatchList'])
 
-def open_page(chromedriver, url) :
+def open_page(chromedriver, url, WindowCount) :
     """Function to call chromedriver and open webpage after authenticating"""
-    driver = webdriver.Chrome(chromedriver)
-    driver.get(url)
-    driver.implicitly_wait(10) # seconds
-    logger.info("Opened webpage %s to read player stats" % url)
-    return driver
+    DriverArray     = [webdriver.Chrome(chromedriver) for i in range(WindowCount)]
+    for i in range(WindowCount):
+        driver = DriverArray[i]
+        driver.get(url)
+        driver.implicitly_wait(10) # seconds
+        logger.info("Opened webpage %s to read player stats using driver number %d",url,i)
+    return DriverArray
 
-def read_data(driver) :
+def go_to_page(driver,page) :
+    if page > 0:
+        Next = driver.find_element_by_xpath('//*[@id="ismr-main"]/div/div[2]/a[1]')
+        Next.click()
+        page = page - 1
+    for i in range(page):
+        Next = driver.find_element_by_xpath('//*[@id="ismr-main"]/div/div[2]/a[3]/div[1]')
+        Next.click()
+    return driver
+            
+
+def read_data(driver,page) :
     try:
         count = 0
-        
-        for page in range(19):                    
-            # Get all the players in the 1st page and print their names. Should modify the loop below so that it extract the other information as well and stores in a table
-            players = driver.find_elements_by_xpath("//*[@id='ismr-main']/div/div[1]/table/tbody/tr")
-            #for pno in range(len(players)):
-            for pno in [0]:
-                Button = driver.find_element_by_xpath("//*[@id='ismr-main']/div/div[1]/table/tbody/tr["+str(pno+1)+"]/td[1]/a")
-                Button.click()
-                name = driver.find_element_by_xpath('//*[@id="ismjs-dialog-title"]')
-                StatusExists = len(driver.find_elements_by_class_name("ism-element-status-bar__content")) #Check if a player is suspended or injured. Change the xpath accordingly
-                pdata = [driver.find_element_by_xpath(value[0]+str(value[1]+StatusExists)+value[2]).text.encode('ascii', 'ignore') for key, value in XPATH.items()]
+        df = pd.DataFrame(columns=list(XPATH.keys())+MatchData)
 
-                DataTable = driver.find_element_by_xpath("//*[@id='ismr-element-history-this']/div/div/table")
-                TableRows = DataTable.find_elements_by_xpath("//*[@id='ismr-element-history-this']/div/div/table/tbody/tr")            
-                for row in range(len(TableRows)):
-                    TableRow  = TableRows[row]
-                    TableCols = TableRow.find_elements_by_xpath("//*[@id='ismr-element-history-this']/div/div/table/tbody/tr["+str(row+1)+"]/td")
-                    mdata= [pno] + [TableRow.find_element_by_xpath("//*[@id='ismr-element-history-this']/div/div/table/tbody/tr["+str(row+1)+"]/td["+str(col+1)+"]").text.encode('ascii', 'ignore') for col in range(len(TableCols))]
-                    df.loc[count] = pdata + mdata
-                    count += 1
-                logger.info("Printed %d rows for %s", len(TableRows),name.text)
-                close = driver.find_element_by_xpath('//*[@id="ismr-element"]/div/div[1]/a')
-                close.click()
+        #Go to the correct page number
+        driver = go_to_page(driver,page)
 
-            if page == 0:
-                Next = driver.find_element_by_xpath('//*[@id="ismr-main"]/div/div[2]/a[1]')
-                Next.click()
-            elif page < 18:
-                Next = driver.find_element_by_xpath('//*[@id="ismr-main"]/div/div[2]/a[3]/div[1]')
-                Next.click()
-                
-                
+        # Get all the players in the 1st page and print their names. Should modify the loop below so that it extract the other information as well and stores in a table
+        players = driver.find_elements_by_xpath("//*[@id='ismr-main']/div/div[1]/table/tbody/tr")
+        for pno in range(len(players)):
+        #for pno in [0,1,2,3,4]:
+            Button = driver.find_element_by_xpath("//*[@id='ismr-main']/div/div[1]/table/tbody/tr["+str(pno+1)+"]/td[1]/a")
+            Button.click()
+            name = driver.find_element_by_xpath('//*[@id="ismjs-dialog-title"]')
+            print(name.text)
+            StatusExists    = len(driver.find_elements_by_class_name("ism-element-status-bar__content")) #Check if a player is suspended or injured. Change the xpath accordingly
+            pdata           = [driver.find_element_by_xpath(value[0]+str(value[1]+StatusExists)+value[2]).text.encode('ascii', 'ignore') for key, value in XPATH.items()]
 
+            TableSize = 23
+            Table     = driver.find_elements_by_xpath("//*[@id='ismr-element-history-this']/div/div/table//tr//td")
+            ElemCount = 0
+            RowData   = []
+            for elem in Table:
+                RowData.append(elem.text)
+                if ElemCount < (TableSize-1) :
+                    ElemCount += 1
+                else:
+                    mdata           = [pno,name.text] + RowData
+                    df.loc[count]   = pdata + mdata
+                    count          += 1
+                    RowData         = []
+                    ElemCount       = 0
+
+            logger.info("Printed %d rows for %s, no %d", len(Table)/TableSize, name.text, pno)
+
+            close = driver.find_element_by_xpath('//*[@id="ismr-element"]/div/div[1]/a')
+            close.click()
 
     finally:
-        return df
+        writer = pd.ExcelWriter('Test'+str(page)+'.xlsx')
+        df.to_excel(writer,'Sheet')
+        writer.save()
+        print(page)
+        #return df
 
 
-driver = open_page(chromedriver, url)
-df = read_data(driver)
-print(df)
+
+DriverArray = open_page(chromedriver, url,WindowCount)
+for i in range(Initial,Final+1):
+    t = threading.Thread(target=read_data, args = (DriverArray[i-Initial],i))
+    t.daemon = True
+    t.start()
+ #DriverArray = [read_data(DriverArray[i],i) for i in range(5)]
